95aba0c530be594e889170d5145da5abb0d3aad1
canyos <4581974@naver.com>
2024-07-30 20:22:38 +0900

    7.30 result추가

--- backend/src/main/java/org/example/controller/UploadController.java
+++ backend/src/main/java/org/example/controller/UploadController.java
@@ -1,5 +1,6 @@
 package org.example.controller;
 
+import org.example.dto.ResponseDto;
 import org.example.service.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,23 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.List;
 
 @Controller
 public class UploadController {
     @Autowired
     UploadService uploadService;
-
-    private final String UPLOAD_DIR_VIDEO = "src/main/frontend/uploads/video/";
-    private final String UPLOAD_DIR_IMAGE = "src/main/frontend/uploads/image/";
 
     @GetMapping("/")
     public String mainPage(){
@@ -35,20 +27,18 @@ public class UploadController {
     @PostMapping("/api/upload/image")
     public ResponseEntity<String> uploadImage(@RequestParam("imageFile") MultipartFile imageFile) {
         try {
-            saveImage(imageFile);
+            uploadService.uploadImage(imageFile);
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일을 저장할 수 없음");
         }
-        uploadService.script();
 
         return ResponseEntity.ok("정상 업로드");
     }
 
     @PostMapping("/api/upload/video")
     public ResponseEntity<String> uploadVideo(@RequestParam("videoFile") MultipartFile videoFile) {
-
         try {
-            saveVideo(videoFile);
+            uploadService.uploadVideo(videoFile);
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("파일을 저장할 수 없음");
         }
@@ -57,88 +47,32 @@ public class UploadController {
 
     @GetMapping("/api/get/image")
     public ResponseEntity<String> getImagePath() {
+        String imagePath;
         try {
-            Path imageDir = Paths.get(UPLOAD_DIR_IMAGE);
-            Path imagePath = Files.list(imageDir)
-                    .filter(Files::isRegularFile)
-                    .findFirst()
-                    .orElseThrow(() -> new RuntimeException("No image file found"));
-            String returnPath = imagePath.toString();
-            int index = returnPath.indexOf("uploads");
-            return new ResponseEntity<>(returnPath.substring(index), HttpStatus.OK);
+            imagePath = uploadService.getImagePath();
         } catch (Exception e) {
             return new ResponseEntity<>("Failed to get image path", HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>(imagePath, HttpStatus.OK);
     }
 
     @GetMapping("/api/get/video")
     public ResponseEntity<String> getVideoPath() {
+        String videoPath;
         try {
-            Path viedoDir = Paths.get(UPLOAD_DIR_VIDEO);
-            Path viedoPath = Files.list(viedoDir)
-                    .filter(Files::isRegularFile)
-                    .findFirst()
-                    .orElseThrow(() -> new RuntimeException("No image file found"));
-            String returnPath = viedoPath.toString();
-            int index = returnPath.indexOf("uploads");
-
-            return new ResponseEntity<>(returnPath.substring(index), HttpStatus.OK);
+            videoPath = uploadService.getVideoPath();
         } catch (Exception e) {
             return new ResponseEntity<>("Failed to get image path", HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>(videoPath, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/result")
+    public ResponseEntity<List<ResponseDto>> getResult(){
+        List<ResponseDto> responseDtoList = uploadService.getResult();
+        return new ResponseEntity<>(responseDtoList, HttpStatus.OK);
     }
 
 
-    private void saveVideo(MultipartFile file) throws IOException {
-        String fileName = file.getOriginalFilename();
-        Path targetPath = Paths.get(UPLOAD_DIR_VIDEO + fileName);
-        Path oldFilePath = null;
-
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(UPLOAD_DIR_IMAGE))) {
-            for (Path path : stream) {
-                if (Files.isRegularFile(path)) {
-                    oldFilePath = path;
-                    break;
-                }
-            }
-        }
-
-        try {
-            byte[] bytes = file.getBytes();
-            Files.write(targetPath, bytes);
-
-            if (oldFilePath != null && !oldFilePath.equals(targetPath)) {
-                Files.delete(oldFilePath);
-            }
-        } catch (IOException e) {
-            throw e;
-        }
-    }
-
-    private void saveImage(MultipartFile file) throws IOException {
-        String fileName = file.getOriginalFilename();
-        Path targetPath = Paths.get(UPLOAD_DIR_IMAGE + fileName);
-        Path oldFilePath = null;
-
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(UPLOAD_DIR_IMAGE))) {
-            for (Path path : stream) {
-                if (Files.isRegularFile(path)) {
-                    oldFilePath = path;
-                    break;
-                }
-            }
-        }
-
-        try {
-            byte[] bytes = file.getBytes();
-            Files.write(targetPath, bytes);
-
-            if (oldFilePath != null && !oldFilePath.equals(targetPath)) {
-                Files.delete(oldFilePath);
-            }
-        } catch (IOException e) {
-            throw e;
-        }
-    }
 
 }
--- backend/src/main/java/org/example/service/UploadService.java
+++ backend/src/main/java/org/example/service/UploadService.java
@@ -1,18 +1,31 @@
 package org.example.service;
 
+import org.apache.coyote.Response;
+import org.example.dto.ResponseDto;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class UploadService {
-    public void script(){
+    private final String UPLOAD_DIR_VIDEO = "src/main/frontend/uploads/video/";
+    private final String UPLOAD_DIR_IMAGE = "src/main/frontend/uploads/image/";
+    private List<ResponseDto> script(){
         StringBuilder output = new StringBuilder();
-
+        List<ResponseDto> response = new ArrayList<>();
         try {
             // Python 스크립트 실행 명령어
-            String command = "python3 script.py"; // Windows 사용 시 "python script.py"로 변경
+            String command = "python script.py"; // Windows 사용 시 "python script.py"로 변경
 
             // 프로세스 빌더 설정
             ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
@@ -25,13 +38,16 @@ public class UploadService {
             // 프로세스 출력 읽기
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
+
             while ((line = reader.readLine()) != null) {
                 output.append(line).append("\n");
+                System.out.println(line);
+                List<String> str = Arrays.stream(line.split(",")).toList();
+                response.add(new ResponseDto(str.get(0),Integer.parseInt(str.get(1))));
             }
 
             // 프로세스 종료 대기
             int exitCode = process.waitFor();
-            output.append("Exited with code: ").append(exitCode);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -39,5 +55,71 @@ public class UploadService {
         }
 
         System.out.println(output);
+        return response;
+    }
+
+    public void uploadImage(MultipartFile imageFile) throws IOException {
+        try {
+            saveFile(UPLOAD_DIR_IMAGE, imageFile);
+        } catch (IOException e) {
+            throw new IOException("이미지 저장 실패");
+        }
+    }
+
+    private void saveFile(String dir, MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        Path targetPath = Paths.get(dir + fileName);
+        Path oldFilePath = null;
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(dir))) {
+            for (Path path : stream) {
+                if (Files.isRegularFile(path)) {
+                    oldFilePath = path;
+                    break;
+                }
+            }
+        }
+
+        try {
+            byte[] bytes = file.getBytes();
+            Files.write(targetPath, bytes);
+
+            if (oldFilePath != null && !oldFilePath.equals(targetPath)) {
+                Files.delete(oldFilePath);
+            }
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    public void uploadVideo(MultipartFile videoFile) throws IOException{
+        try{
+            saveFile(UPLOAD_DIR_VIDEO, videoFile);
+        }catch(IOException e){
+            throw new IOException("저장 실패");
+        }
+    }
+
+    public String getImagePath() throws IOException {
+        return getPath(UPLOAD_DIR_IMAGE);
+    }
+
+    public String getVideoPath() throws IOException {
+        return getPath(UPLOAD_DIR_VIDEO);
+    }
+
+    private String getPath(String dir) throws IOException {
+        Path viedoDir = Paths.get(dir);
+        Path viedoPath = Files.list(viedoDir)
+                .filter(Files::isRegularFile)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No image file found"));
+        String returnPath = viedoPath.toString();
+        int index = returnPath.indexOf("uploads");
+        return returnPath.substring(index);
+    }
+
+    public List<ResponseDto> getResult() {
+        return script();
     }
 }
