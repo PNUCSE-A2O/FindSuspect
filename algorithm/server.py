06a28d1d42cca903bc635c5872e0285eafa978cf
canyos <31244128+canyos@users.noreply.github.com>
2024-08-12 21:30:07 +0900

    Update server.py

--- algorithm/server.py
+++ algorithm/server.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import shutil
 
 def run_python_file(file_path):
     # 파일이 있는 디렉토리로 이동
@@ -18,6 +19,20 @@ def run_python_file(file_path):
         # 원래 디렉토리로 돌아감
         os.chdir(original_dir)
 
+def move_files(src_dir, dst_dir):
+    # 목적지 디렉토리가 존재하면 삭제
+    if os.path.exists(dst_dir):
+        shutil.rmtree(dst_dir)
+    
+    # 목적지 디렉토리 새로 생성
+    os.makedirs(dst_dir)
+    
+    # 소스 디렉토리의 모든 파일을 목적지 디렉토리로 이동
+    for item in os.listdir(src_dir):
+        s = os.path.join(src_dir, item)
+        d = os.path.join(dst_dir, item)
+        shutil.move(s, d)
+
 # 현재 디렉토리 경로
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -25,6 +40,10 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 a_path = os.path.join(current_dir, 'video_to_crop', 'crop.py')
 b_path = os.path.join(current_dir, 'VTFPAR++', 'all_image_test.py')
 
+src_folder = os.path.join(current_dir, 'video_to_crop', 'output_images')
+dst_folder = os.path.join(current_dir, 'VTFPAR++', 'person_snapshots')
+
 # Python 파일 실행
 run_python_file(a_path)
+move_files(src_folder, dst_folder)
 run_python_file(b_path)
